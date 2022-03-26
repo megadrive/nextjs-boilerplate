847993dd749d2351a337f61904f08d306359b775
Andy <a.simeon@gmail.com>
2022-03-26 21:27:12 +1100

    SignInButton

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,13 +1,23 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 
+const SignIn = () => {
+  return <Link href="/api/auth/signin">Log In</Link>;
+};
+
+const SignOut = (props: { name: string }) => {
+  const { name } = props;
+  return (
+    <span>
+      {name} <Link href="/api/auth/signout">Log Out</Link>
+    </span>
+  );
+};
+
 export const SignInButton = () => {
   const { data, status } = useSession();
+  const name = data?.user?.name;
   const loggedIn = status === "authenticated";
 
-  return (
-    <Link href={!loggedIn ? "/api/auth/signin" : "/api/auth/signout"}>
-      <a>{!loggedIn ? "Sign In" : data?.user?.name}</a>
-    </Link>
-  );
+  return !loggedIn ? <SignIn /> : <SignOut name={name ? name : "Unknown"} />;
 };
