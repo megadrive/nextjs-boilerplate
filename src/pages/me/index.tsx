dc1095a1546563ddb475db88fde9c5564bf0181e
Andy <a.simeon@gmail.com>
2022-06-15 22:16:58 +1000

    Smol /me

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,4 +1,25 @@
+import { Skeleton } from "@mui/material";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-export const Me = () => {
-    return <div></div>;
+const Me = () => {
+  const router = useRouter();
+  const { status, data } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // Redirect to signin page if not authenticated
+      router.push("/signin");
+    },
+  });
+  return (
+    <>
+      {status === "loading" ? (
+        <Skeleton variant="text" width={300} height={10} />
+      ) : (
+        <div>Your name is {data.user?.name}</div>
+      )}
+    </>
+  );
 };
+
+export default Me;
